fbc637aae77c5337f91570ee09ee75ee62bc1c48
dhairya22157 <dhairya22157@iiitd.ac.in>
2024-08-03 20:21:39 +0530

    error commit

--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
 
 import  connectDB  from "./db/index.js";
 import dotenv from "dotenv";
+
 dotenv.config({
     path: './env'
 });
-connectDB();
+connectDB().then(()=>{
+    console.log("Connected to MongoDB");
+    app.listen(process.env.PORT||8000,()=>{
+        console.log(`Server is running on port ${process.env.PORT}`);
+    })
+})
+.catch((error)=>{
+    console.log("Error: ",error);
+    
+})
 
 // (async()=>{
 //     try{
