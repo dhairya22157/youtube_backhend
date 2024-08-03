855827da28664db8c0d742b5772ce9808a5ba554
dhairya22157 <dhairya22157@iiitd.ac.in>
2024-08-03 16:26:04 +0530

    mongo db connected

---
+++ src/db/index.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+import express from "express";
+import {DB_NAME} from "../constants.js";
+const app = express();
+const connectDB = async()=>{
+    try{
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
+        console.log(`Connected to MongoDB db host:${connectionInstance.connection.host}`);
+        // app.listen(process.env.PORT,()=>{
+        //     console.log(`Server is running on port ${process.env.PORT}`);
+        // })
+    }
+    catch(error){
+        console.log("mongo db connection error ", error);
+        throw error;
+    }
+}
+export default connectDB;
---
+++ src/index.js
@@ -0,0 +1,24 @@
+
+import  connectDB  from "./db/index.js";
+import dotenv from "dotenv";
+dotenv.config({
+    path: './env'
+});
+connectDB();
+
+// (async()=>{
+//     try{
+//         await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
+//         app.on("error",(error)=>{
+//             console.log(`Error: ${error}`);
+//             throw error;
+//         })
+//         app.listen(process.env.PORT,()=>{
+//             console.log(`Server is running on port ${process.env.PORT}`);
+//         })
+//     }
+//     catch(error){
+//         console.log(`Error: ${error}`);
+//         throw error;
+//     }
+// })
